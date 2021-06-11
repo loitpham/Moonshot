915e79e821a573189956d8562988246d61b1860f
Loi Pham <loitpham@gmail.com>
2021-06-11 00:20:03 -0700

    Made the Decoder generic.

---
+++ Moonshot/Moonshot/Mission.swift
@@ -0,0 +1,20 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Loi Pham on 6/10/21.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
